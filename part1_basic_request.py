0b936da52acdb64f481978b14a0e93eb2d83cb52
Anurag Ganesh Patil <anuragpatil060@gmail.com>
2026-01-18 17:52:02 +0530

    complete Exercise get request

--- part1_basic_request.py
+++ part1_basic_request.py
@@ -11,10 +11,12 @@ We'll use JSONPlaceholder - a free fake API for testing.
 import requests
 
 # Step 1: Define the API URL
-url = "https://jsonplaceholder.typicode.com/posts/1"
+url = "https://jsonplaceholder.typicode.com/posts/5"
 
 # Step 2: Make a GET request
 response = requests.get(url)
+print(type(response))
+print(response)
 
 # Step 3: Print the response
 print("=== Basic API Request ===\n")
@@ -22,6 +24,42 @@ print(f"URL: {url}")
 print(f"Status Code: {response.status_code}")
 print(f"\nResponse Data:")
 print(response.json())
+
+
+url = "https://api.coinpaprika.com/v1/coins/btc-bitcoin"
+response1 = requests.get(url)
+print(type(response1))
+print(response1)
+
+print(response1.json())
+
+url = "https://api.open-meteo.com/v1/forecast"
+
+params = {"latitude":28.61,
+          "longitude":77.23,
+          "current_weather":"true"}
+
+headers = {"Accept": "application/json"}
+
+response = requests.get(url,params=params,headers=headers)
+
+print(response.status_code)
+print(response.json())
+print("Current temp:",response.json()['current_weather']['temperature'])
+
+url = "https://jsonplaceholder.typicode.com/users"
+
+response4 = requests.get(url)
+print(len(response4.json()))
+
+url = "https://jsonplaceholder.typicode.com/posts/999"
+
+response5 = requests.get(url)
+print(response5.json())
+
+# for user in response4:
+#     print(user[0]['name'])
+
 
 
 # --- EXERCISES ---
@@ -35,3 +73,4 @@ print(response.json())
 #
 # Exercise 3: What happens if you fetch a post that doesn't exist?
 #             Try: https://jsonplaceholder.typicode.com/posts/999
+ #            Ans: It has shown an empty dictionary {}
